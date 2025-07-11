c3ebb6d692de0e5a281bc8ad34f19daa01ccd5de
Guilherme Cordeiro <guitocordeiro2003@gmail.com>
2025-07-11 16:54:31 -0300

    Criação do projeto em Java com Spring Boot

---
+++ project_java_springboot/src/main/java/com/guicr3/project_java_springboot/model/Greeting.java
@@ -0,0 +1,3 @@
+package com.guicr3.project_java_springboot.model;
+
+public record Greeting (long id, String content){}
---
+++ project_java_springboot/src/test/java/com/guicr3/project_java_springboot/StartupTests.java
@@ -0,0 +1,13 @@
+package com.guicr3.project_java_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
