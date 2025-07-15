0cf0c1a794b28d826f98338676d35acbb8e51957
Guilherme Cordeiro <guitocordeiro2003@gmail.com>
2025-07-15 17:45:41 -0300

    Adicionando Loggers de INFO para facilitar o debug

--- project_java_springboot/src/main/java/com/guicr3/project_java_springboot/services/PersonServices.java
+++ project_java_springboot/src/main/java/com/guicr3/project_java_springboot/services/PersonServices.java
@@ -3,6 +3,8 @@ package com.guicr3.project_java_springboot.services;
 import com.guicr3.project_java_springboot.exception.ResourceNotFoundException;
 import com.guicr3.project_java_springboot.model.Person;
 import com.guicr3.project_java_springboot.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,27 +16,32 @@ public class PersonServices {
     @Autowired
     private PersonRepository repository;
 
-    //private final Logger logger = Logger.getLogger(PersonServices.class.getName());
+    private final Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
 
     public List<Person> findAll(){
+        logger.info("Finding all users");
         return repository.findAll();
     }
 
     public Person findByID(Long id){
+        logger.info("Finding one Person");
         return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records founds for this id"));
     }
 
     public Person create(Person person){
+        logger.info("Creating a person");
         return repository.save(person);
     }
 
     public void update(Person person){
+        logger.info("Updating a person");
         Person newPerson = findByID(person.getId());
         updateData(newPerson, person);
         repository.save(newPerson);
     }
 
     public void delete(Long id){
+        logger.info("Deleting a person");
         Person person = findByID(id);
         repository.delete(person);
     }
