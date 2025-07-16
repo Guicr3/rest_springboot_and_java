3395e920ab8c065110046062907679021faf08a5
Guilherme Cordeiro <guitocordeiro2003@gmail.com>
2025-07-16 18:38:28 -0300

    Adicionando implementando content negotiation para retorno não só de JSON, mas também, XML e YAML

--- project_java_springboot/src/main/java/com/guicr3/project_java_springboot/controllers/PersonController.java
+++ project_java_springboot/src/main/java/com/guicr3/project_java_springboot/controllers/PersonController.java
@@ -3,6 +3,7 @@ package com.guicr3.project_java_springboot.controllers;
 import com.guicr3.project_java_springboot.data.dto.PersonDTO;
 import com.guicr3.project_java_springboot.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,29 +16,29 @@ public class PersonController {
     @Autowired
     private PersonServices service;
 
-        @GetMapping(value="/{id}")
+        @GetMapping(value="/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
         private PersonDTO findById(@PathVariable("id") Long id){
             return service.findByID(id);
         }
 
-        @GetMapping
+        @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
         private ResponseEntity<List<PersonDTO>> findAll(){
             return ResponseEntity.ok().body(service.findAll());
         }
 
-        @PostMapping
+        @PostMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
         private ResponseEntity<PersonDTO> create(@RequestBody PersonDTO person){
             return ResponseEntity.ok().body(service.create(person));
         }
 
-        @PutMapping(value="/{id}")
+        @PutMapping(value="/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
         private ResponseEntity<Void> update(@RequestBody PersonDTO updatedPerson, @PathVariable Long id){
             updatedPerson.setId(id);
             service.update(updatedPerson);
             return ResponseEntity.noContent().build();
         }
 
-        @DeleteMapping(value="/{id}")
+        @DeleteMapping(value="/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
         private ResponseEntity<Void> delete(@PathVariable("id") Long id){
             service.delete(id);
             return ResponseEntity.noContent().build();
---
+++ project_java_springboot/src/main/java/com/guicr3/project_java_springboot/serialization/converter/YamlJackson2HttpMessageConverter.java
@@ -0,0 +1,14 @@
+package com.guicr3.project_java_springboot.serialization.converter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+
+public final class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+    YamlJackson2HttpMessageConverter() {
+        super(new YAMLMapper()
+                  .setSerializationInclusion(JsonInclude.Include.NON_NULL),
+                    MediaType.parseMediaType("application/x-yaml"));
+    }
+}
